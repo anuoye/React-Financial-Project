88c5f1f9b219c64c041f7771d9b997c9e4fea5e1
anuoye <anuoyebanjodunmi660@gmail.com>
2022-10-19 05:49:41 +0100

    first commit

---
+++ src/components/Hero.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Hero.css'
+
+const Hero = () => {
+    return (
+        <div className='hero'>
+            <div className='content'>
+                <p>Get In Touch</p>
+                <p>9-6700-123-4567</p>
+                <p>Financial Intelligence and Money</p>
+                <p>Doesn't come with instructions</p>
+                <button href='/' className='button'>Free Consultation</button>
+            </div>
+        </div>
+    )
+}
+
+export default Hero
